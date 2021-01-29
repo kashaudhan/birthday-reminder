031060efcf3b0c0839ff56d938957a9fb27b606c
Dharmendra Kashaudhan <47530882+kashaudhan@users.noreply.github.com>
2021-01-29 19:45:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import data from "./data";
-import List from "./list";
 import { useState } from "react";
 
 function App() {
   const [people, setPeople] = useState(data);
+
+  const removePerson = (id) => {
+    setPeople(() => {
+      let newPeopel = people.filter((p) => p.id !== id);
+      return newPeopel;
+    });
+  };
+
   return (
     <main>
       <section className="container">
         <h3>{people.length} birthdays today</h3>
-        <List data={people} />
+        <div>
+          {people.map((person) => {
+            const { id, name, age, image } = person;
+            return (
+              <article className="person" key={id}>
+                <img src={image} alt={name} />
+                <div>
+                  <h4>{name}</h4>
+                  <button className="remove" onClick={() => removePerson(id)}>
+                    remove
+                  </button>
+                  <p>{age} years</p>
+                </div>
+              </article>
+            );
+          })}
+        </div>
         <button className="clr-btn" onClick={() => setPeople([])}>
           Clear All
         </button>
